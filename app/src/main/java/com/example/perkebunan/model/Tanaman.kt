22692fb90d7ad9056b45da72ea9376ee7b8a5a95
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-20 15:15:57 +0700

    membuat data class Tanaman

---
+++ app/src/main/java/com/example/perkebunan/model/Tanaman.kt
@@ -0,0 +1,16 @@
+package com.example.perkebunan.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Tanaman(
+    @SerialName("id_tanaman")
+    val idTanaman: String,
+    @SerialName("nama_tanaman")
+    val namaTanaman: String,
+    @SerialName("periode_tanam")
+    val periodeTanam: String,
+    @SerialName("deskripsi_tanaman")
+    val deskripsiTanaman: String,
+)
