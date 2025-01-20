e5cd4a2a85ba054510a353e27626decb69b23fb3
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-20 18:15:55 +0700

    membuat fun EntryTnmScreen dan object DestinasiEntryTanaman

--- app/src/main/java/com/example/perkebunan/ui/view/tanaman/InsertTanamanView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/tanaman/InsertTanamanView.kt
@@ -4,19 +4,74 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.perkebunan.navigation.DestinasiNavigasi
+import com.example.perkebunan.ui.PenyediaViewModel
+import com.example.perkebunan.ui.customwidget.CostumeTopAppBar
 import com.example.perkebunan.ui.viewmodel.tanaman.InsertTanamanUiEvent
 import com.example.perkebunan.ui.viewmodel.tanaman.InsertTanamanUiState
+import com.example.perkebunan.ui.viewmodel.tanaman.InsertTanamanViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiEntryTanaman: DestinasiNavigasi {
+    override val route = "item_entryTanaman"
+    override val titleRes = "Entry Tanaman"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryTnmScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertTanamanViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryTanaman.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerPadding ->
+        EntryBody(
+            insertTanamanUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertTnmState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertTnm()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntryBody(
