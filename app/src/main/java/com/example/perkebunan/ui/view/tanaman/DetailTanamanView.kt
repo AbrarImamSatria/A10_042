5e76747a0cb231b9e624c79042c0a3ff97351ff9
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-20 18:47:27 +0700

    membuat fun DetailTanamanStatus

--- app/src/main/java/com/example/perkebunan/ui/view/tanaman/DetailTanamanView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/tanaman/DetailTanamanView.kt
@@ -3,6 +3,8 @@ package com.example.perkebunan.ui.view.tanaman
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -18,6 +20,23 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.perkebunan.R
 import com.example.perkebunan.model.Tanaman
+import com.example.perkebunan.ui.viewmodel.tanaman.DetailTanamanUiState
+
+@Composable
+private fun DetailTanamanStatus(
+    detailTanamanUiState: DetailTanamanUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    when (detailTanamanUiState) {
+        is DetailTanamanUiState.Loading -> OnLoadingDetail(modifier = modifier.fillMaxSize())
+        is DetailTanamanUiState.Success -> DetailTanamanLayout(
+            tanaman = detailTanamanUiState.tanaman,
+            modifier = modifier.fillMaxWidth()
+        )
+        is DetailTanamanUiState.Error -> OnErrorDetail(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
 
 @Composable
 private fun OnErrorDetail(retryAction: () -> Unit, modifier: Modifier = Modifier) {
