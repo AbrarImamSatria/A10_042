46cc6f3f28123da76c0c422fd4c9460e34055ef8
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-20 19:57:23 +0700

    membuat fun EditScreenTanaman dan object DestinasiEditTanaman

--- app/src/main/java/com/example/perkebunan/ui/view/tanaman/UpdateTanamanView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/tanaman/UpdateTanamanView.kt
@@ -4,19 +4,83 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.perkebunan.navigation.DestinasiNavigasi
+import com.example.perkebunan.ui.PenyediaViewModel
+import com.example.perkebunan.ui.customwidget.CostumeTopAppBar
 import com.example.perkebunan.ui.viewmodel.tanaman.UpdateTanamanUiEvent
 import com.example.perkebunan.ui.viewmodel.tanaman.UpdateTanamanUiState
+import com.example.perkebunan.ui.viewmodel.tanaman.UpdateTanamanViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiEditTanaman : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = "Edit Tnm"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditScreenTanaman(
+    navigateBack: () -> Unit,
+    onNavigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateTanamanViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    idTanaman: String
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(key1 = true) {
+        coroutineScope.launch {
+            viewModel.getTanamanbyIdTanaman(idTanaman)
+        }
+    }
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEditTanaman.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EditBodyTanaman(
+            updateTanamanUiState = viewModel.uiState,
+            onTnmValueChange = viewModel::updateTanamanUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateTnm(idTanaman)
+                    onNavigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EditBodyTanaman(
