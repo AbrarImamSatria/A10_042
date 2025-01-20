0c439f2cff240f4e52c1f46e3dbf4e9c1b1722fd
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-20 20:09:22 +0700

    memanggil fun PerkebunanApp

--- app/src/main/java/com/example/perkebunan/MainActivity.kt
+++ app/src/main/java/com/example/perkebunan/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.perkebunan.ui.PerkebunanApp
 import com.example.perkebunan.ui.theme.PerkebunanTheme
+import com.example.perkebunan.ui.view.tanaman.HomeTanamanScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +21,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PerkebunanTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                PerkebunanApp()
                 }
             }
         }
     }
-}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
