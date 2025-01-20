0d2062939d027244f6bc8f584bbdb913d0a3ca0e
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-20 19:29:14 +0700

    membuat fun DetailScreenTanaman dan object DestinasiDetailTanaman

--- app/src/main/java/com/example/perkebunan/ui/view/tanaman/DetailTanamanView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/tanaman/DetailTanamanView.kt
@@ -7,20 +7,86 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.perkebunan.R
 import com.example.perkebunan.model.Tanaman
+import com.example.perkebunan.navigation.DestinasiNavigasi
+import com.example.perkebunan.ui.PenyediaViewModel
+import com.example.perkebunan.ui.customwidget.CostumeTopAppBar
 import com.example.perkebunan.ui.viewmodel.tanaman.DetailTanamanUiState
+import com.example.perkebunan.ui.viewmodel.tanaman.DetailTanamanViewModel
+
+object DestinasiDetailTanaman : DestinasiNavigasi {
+    override val route = "detail/{idTanaman}"
+    override val titleRes = "Detail Tanaman"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreenTanaman(
+    navigateBack: () -> Unit,
+    navigateToEdit: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    idTanaman: String,
+    viewModel: DetailTanamanViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    LaunchedEffect(idTanaman) {
+        viewModel.getTanamanbyIdTanaman(idTanaman)
+    }
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetailTanaman.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack,
+                onRefresh = { viewModel.getTanamanbyIdTanaman(idTanaman) }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navigateToEdit(idTanaman) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa"
+                )
+            }
+        }
+    ) { innerPadding ->
+        DetailTanamanStatus(
+            detailTanamanUiState = viewModel.tnmDetailUiState,
+            retryAction = { viewModel.getTanamanbyIdTanaman(idTanaman) },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
 
 @Composable
 private fun DetailTanamanStatus(
@@ -84,19 +150,19 @@ fun DetailTanamanLayout(
         ) {
             Text(
                 text = "ID Tanaman: ${tanaman.idTanaman}",
-                style = MaterialTheme.typography.titleLarge
+                style = MaterialTheme.typography.titleMedium
             )
             Text(
                 text = "Nama Tanaman: ${tanaman.namaTanaman}",
-                style = MaterialTheme.typography.titleLarge
+                style = MaterialTheme.typography.titleMedium
             )
             Text(
                 text = "Periode Tanaman: ${tanaman.periodeTanam}",
-                style = MaterialTheme.typography.titleLarge
+                style = MaterialTheme.typography.titleMedium
             )
             Text(
                 text = "Deskripsi Tanaman: ${tanaman.deskripsiTanaman}",
-                style = MaterialTheme.typography.titleLarge
+                style = MaterialTheme.typography.titleMedium
             )
         }
     }
