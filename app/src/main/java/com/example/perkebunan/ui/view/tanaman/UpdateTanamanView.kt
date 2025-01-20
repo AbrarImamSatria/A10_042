ab308431eb6a72f53806e5b0488b76fc04e4551a
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-20 19:53:17 +0700

    membuat fun FormInputTanamanEdit

---
+++ app/src/main/java/com/example/perkebunan/ui/view/tanaman/UpdateTanamanView.kt
@@ -0,0 +1,73 @@
+package com.example.perkebunan.ui.view.tanaman
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.perkebunan.ui.viewmodel.tanaman.UpdateTanamanUiEvent
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputTanamanEdit(
+    updateTanamanUiEvent: UpdateTanamanUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (UpdateTanamanUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = updateTanamanUiEvent.idTanaman,
+            onValueChange = { onValueChange(updateTanamanUiEvent.copy(idTanaman = it)) },
+            label = { Text("ID Tanaman") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateTanamanUiEvent.namaTanaman,
+            onValueChange = { onValueChange(updateTanamanUiEvent.copy(idTanaman = it)) },
+            label = { Text("ID Tanaman") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateTanamanUiEvent.periodeTanam,
+            onValueChange = { onValueChange(updateTanamanUiEvent.copy(idTanaman = it)) },
+            label = { Text("ID Tanaman") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateTanamanUiEvent.deskripsiTanaman,
+            onValueChange = { onValueChange(updateTanamanUiEvent.copy(idTanaman = it)) },
+            label = { Text("ID Tanaman") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled) {
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
