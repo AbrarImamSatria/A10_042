db61f3e27486bdfd02ee00ad270d0e81a012d841
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-20 20:07:35 +0700

    mendaftarkan viewmodel di PenyediaViewModel

---
+++ app/src/main/java/com/example/perkebunan/ui/PenyediaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.perkebunan.ui
+
+import android.text.Editable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.perkebunan.PerkebunanApplications
+import com.example.perkebunan.ui.viewmodel.tanaman.DetailTanamanViewModel
+import com.example.perkebunan.ui.viewmodel.tanaman.HomeTanamanViewModel
+import com.example.perkebunan.ui.viewmodel.tanaman.InsertTanamanViewModel
+import com.example.perkebunan.ui.viewmodel.tanaman.UpdateTanamanViewModel
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeTanamanViewModel(aplikasiPerkebunan().container.tanamanRepository) }
+        initializer { InsertTanamanViewModel(aplikasiPerkebunan().container.tanamanRepository) }
+        initializer { DetailTanamanViewModel(aplikasiPerkebunan().container.tanamanRepository) }
+        initializer { UpdateTanamanViewModel(aplikasiPerkebunan().container.tanamanRepository) }
+    }
+
+    fun CreationExtras.aplikasiPerkebunan():PerkebunanApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as PerkebunanApplications)
+}
