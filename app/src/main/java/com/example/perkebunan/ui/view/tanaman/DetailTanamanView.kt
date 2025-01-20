2214343acae369b5e6f3f1033c2a6922975e6398
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-20 18:46:09 +0700

    membuat fun OnErrorDetail dan fun OnLoadingDetail

--- app/src/main/java/com/example/perkebunan/ui/view/tanaman/DetailTanamanView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/tanaman/DetailTanamanView.kt
@@ -1,16 +1,53 @@
 package com.example.perkebunan.ui.view.tanaman
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.perkebunan.R
 import com.example.perkebunan.model.Tanaman
+
+@Composable
+private fun OnErrorDetail(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.eror),
+            contentDescription = stringResource(R.string.loading_failed)
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+private fun OnLoadingDetail(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
 
 @Composable
 fun DetailTanamanLayout(
