e5be6084176969d839270ee18deae1400b46c37c
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-22 14:48:15 +0700

    membuat class HomePekerjaViewModel

--- app/src/main/java/com/example/perkebunan/ui/viewmodel/pekerja/HomePekerjaViewModel.kt
+++ app/src/main/java/com/example/perkebunan/ui/viewmodel/pekerja/HomePekerjaViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.perkebunan.ui.viewmodel.pekerja
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.perkebunan.model.Pekerja
-import com.example.perkebunan.model.Tanaman
+import com.example.perkebunan.repository.PekerjaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomePekerjaUiState{
     data class Succes(val pekerja: List<Pekerja>):HomePekerjaUiState()
@@ -9,3 +17,36 @@ sealed class HomePekerjaUiState{
     object Loading:HomePekerjaUiState()
 }
 
+class HomePekerjaViewModel(private val pkj: PekerjaRepository): ViewModel(){
+    var pkjUiState: HomePekerjaUiState by mutableStateOf(HomePekerjaUiState.Loading)
+        private set
+
+    init {
+        getPkj()
+    }
+
+    fun getPkj(){
+        viewModelScope.launch {
+            pkjUiState = HomePekerjaUiState.Loading
+            pkjUiState = try {
+                HomePekerjaUiState.Succes(pkj.getPekerja())
+            }catch (e: IOException){
+                HomePekerjaUiState.Error
+            }catch (e: HttpException){
+                HomePekerjaUiState.Error
+            }
+        }
+    }
+
+    fun deletePkj(idPekerja: String){
+        viewModelScope.launch {
+            try {
+                pkj.deletePekerja(idPekerja)
+            }catch (e: IOException){
+                HomePekerjaUiState.Error
+            }catch (e: HttpException){
+                HomePekerjaUiState.Error
+            }
+        }
+    }
+}
