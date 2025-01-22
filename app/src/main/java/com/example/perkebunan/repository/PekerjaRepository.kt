482e7a611b85666104b92af3d50305cda9152145
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-22 14:36:47 +0700

    membuat PekerjaRepository

---
+++ app/src/main/java/com/example/perkebunan/repository/PekerjaRepository.kt
@@ -0,0 +1,55 @@
+package com.example.perkebunan.repository
+
+import com.example.perkebunan.model.Pekerja
+import com.example.perkebunan.model.Tanaman
+import com.example.perkebunan.service_api.PekerjaService
+import com.example.perkebunan.service_api.TanamanService
+import java.io.IOException
+
+interface PekerjaRepository {
+    suspend fun insertPekerja(pekerja: Pekerja)
+
+    suspend fun getPekerja(): List<Pekerja>
+
+    suspend fun updatePekerja(idPekerja: String, pekerja: Pekerja)
+
+    suspend fun deleteTanaman(idPekerja: String)
+
+    suspend fun getTanamanbyIdTanaman(idPekerja: String): Pekerja
+}
+
+class NetworkPekerjaRepository(
+    private val pekerjaApiService: PekerjaService
+) : PekerjaRepository {
+    override suspend fun insertPekerja(pekerja: Pekerja) {
+        pekerjaApiService.insertPekerja(pekerja)
+    }
+
+    override suspend fun getPekerja(): List<Pekerja> =
+        pekerjaApiService.getAllPekerja()
+
+    override suspend fun updatePekerja(idPekerja: String, pekerja: Pekerja) {
+        pekerjaApiService.updatePekerja(idPekerja, pekerja)
+    }
+
+    override suspend fun deleteTanaman(idPekerja: String) {
+        try {
+            val response = pekerjaApiService.deletePekerja(idPekerja)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete pekerja. HTTP Status Code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getTanamanbyIdTanaman(idPekerja: String): Pekerja {
+        return pekerjaApiService.getPekerjabyIdPekerja(idPekerja)
+    }
+}
+
+
