d2892f6fa2034fe6267742a5c5a8730a30f9d7fd
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-22 15:10:24 +0700

    membuat fun pkjcard dan PkjLayout

--- app/src/main/java/com/example/perkebunan/ui/view/pekerja/HomePekerjaView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/pekerja/HomePekerjaView.kt
@@ -1,10 +1,125 @@
 package com.example.perkebunan.ui.view.pekerja
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.perkebunan.R
+import com.example.perkebunan.model.Pekerja
+import com.example.perkebunan.model.Tanaman
+import com.example.perkebunan.ui.view.tanaman.TnmCard
+
+@Composable
+fun PkjLayout(
+    pekerja: List<Pekerja>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Pekerja) -> Unit,
+    onDeleteClick: (Pekerja) -> Unit = {}
+){
+    LazyColumn (
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(pekerja){ pekerja ->
+            PkjCard(
+                pekerja = pekerja,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(pekerja) },
+                onDeleteClick = {
+                    onDeleteClick(pekerja)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun PkjCard(
+    pekerja: Pekerja,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Pekerja) -> Unit = {},
+){
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFECEFF1))
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.farmer2),
+                contentDescription = "Gambar Pekerja",
+                modifier = Modifier.size(60.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = pekerja.namaPekerja,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp)
+                )
+                Text(
+                    text = pekerja.kontakPekerja,
+                    style = MaterialTheme.typography.bodyMedium.copy(color = Color.Gray)
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.info),
+                    contentDescription = "Info Detail",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clickable {}
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.bin),
+                    contentDescription = "Hapus Pekerja",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clickable { onDeleteClick(pekerja) }
+                )
+            }
+        }
+    }
+}
 
 @Composable
 private fun DeleteConfirmationDialog(
