79138c6659f561cdc741de866f11ca433ddb3842
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-22 17:25:54 +0700

    membuat fun EditBodyPekerja

--- app/src/main/java/com/example/perkebunan/ui/view/pekerja/UpdatePekerjaView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/pekerja/UpdatePekerjaView.kt
@@ -4,15 +4,48 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.perkebunan.ui.viewmodel.pekerja.UpdatePekerjaUiEvent
-import com.example.perkebunan.ui.viewmodel.tanaman.UpdateTanamanUiEvent
+import com.example.perkebunan.ui.viewmodel.pekerja.UpdatePekerjaUiState
+
+@Composable
+fun EditBodyPekerja(
+    updatePekerjaUiState: UpdatePekerjaUiState,
+    onPkjValueChange: (UpdatePekerjaUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputPekerjaEdit(
+            updatePekerjaUiEvent = updatePekerjaUiState.updatePekerjaUiEvent,
+            onValueChange = onPkjValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF2CAC7C)
+            )
+        ) {
+            Text(text = "Simpan Perubahan")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
