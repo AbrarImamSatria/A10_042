0db2c287a4e66fb6928d57d4b4d4ac426a79d78f
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-22 16:24:55 +0700

    membuat fun EntryPkjScreen dan object DestinasiEntryPekerja

--- app/src/main/java/com/example/perkebunan/ui/view/pekerja/InsertPekerjaView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/pekerja/InsertPekerjaView.kt
@@ -4,19 +4,76 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.perkebunan.navigation.DestinasiNavigasi
+import com.example.perkebunan.ui.PenyediaViewModel
+import com.example.perkebunan.ui.customwidget.CostumeTopAppBar
 import com.example.perkebunan.ui.viewmodel.pekerja.InsertPekerjaUiEvent
 import com.example.perkebunan.ui.viewmodel.pekerja.InsertPekerjaUiState
+import com.example.perkebunan.ui.viewmodel.pekerja.InsertPekerjaViewModel
+import com.example.perkebunan.ui.viewmodel.tanaman.InsertTanamanViewModel
+import kotlinx.coroutines.launch
+
+
+object DestinasiEntryPekerja: DestinasiNavigasi {
+    override val route = "item_entryPekerja"
+    override val titleRes = "Entry Pekerja"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryPkjScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertPekerjaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryPekerja.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerPadding ->
+        EntryBody(
+            insertPekerjaUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertPkjState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPkj()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntryBody(
