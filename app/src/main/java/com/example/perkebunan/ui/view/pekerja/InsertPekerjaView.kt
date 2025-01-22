34441e6574637654c61a96e46ce6f1bdb2eae019
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-22 16:12:11 +0700

    membuat fun Form input pekerja

---
+++ app/src/main/java/com/example/perkebunan/ui/view/pekerja/InsertPekerjaView.kt
@@ -0,0 +1,72 @@
+package com.example.perkebunan.ui.view.pekerja
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.perkebunan.ui.viewmodel.pekerja.InsertPekerjaUiEvent
+import com.example.perkebunan.ui.viewmodel.tanaman.InsertTanamanUiEvent
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertPekerjaUiEvent: InsertPekerjaUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertPekerjaUiEvent)->Unit = {},
+    enabled: Boolean = true
+) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertPekerjaUiEvent.idPekerja,
+            onValueChange = {onValueChange(insertPekerjaUiEvent.copy(idPekerja = it))},
+            label = { Text("ID Pekerja") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertPekerjaUiEvent.namaPekerja,
+            onValueChange = {onValueChange(insertPekerjaUiEvent.copy(namaPekerja = it))},
+            label = { Text("Nama Pekerja") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertPekerjaUiEvent.jabatan,
+            onValueChange = {onValueChange(insertPekerjaUiEvent.copy(jabatan = it))},
+            label = { Text("Jabatan Pekerja") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertPekerjaUiEvent.kontakPekerja,
+            onValueChange = {onValueChange(insertPekerjaUiEvent.copy(kontakPekerja = it))},
+            label = { Text("Kontak Pekerja") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled){
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding((12.dp))
+        )
+    }
+}
