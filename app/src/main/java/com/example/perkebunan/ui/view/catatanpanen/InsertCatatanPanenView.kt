f4a2d3bd1656cf2512bbfd27a816ac19925fb543
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-23 12:11:01 +0700

    membuat fun EntryBody catatan panen

--- app/src/main/java/com/example/perkebunan/ui/view/catatanpanen/InsertCatatanPanenView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/catatanpanen/InsertCatatanPanenView.kt
@@ -4,15 +4,46 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.perkebunan.ui.viewmodel.catatanpanen.InsertCatatanPanenUiEvent
-import com.example.perkebunan.ui.viewmodel.pekerja.InsertPekerjaUiEvent
+import com.example.perkebunan.ui.viewmodel.catatanpanen.InsertCatatanPanenUiState
+
+@Composable
+fun EntryBody(
+    insertCatatanPanenUiState: InsertCatatanPanenUiState,
+    onSiswaValueChange: (InsertCatatanPanenUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertCatatanPanenUiEvent = insertCatatanPanenUiState.insertCatatanPanenUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF00A67E)),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
