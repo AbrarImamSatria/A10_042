afd6b89b76dade1c95e713814f9054a6c0a3e03d
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-23 12:53:00 +0700

    membuat fun EditBodyCatatanPanen

--- app/src/main/java/com/example/perkebunan/ui/view/catatanpanen/UpdateCatatanPanenView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/catatanpanen/UpdateCatatanPanenView.kt
@@ -4,14 +4,48 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.perkebunan.ui.viewmodel.catatanpanen.UpdateCatatanPanenUiEvent
+import com.example.perkebunan.ui.viewmodel.catatanpanen.UpdateCatatanPanenUiState
+
+@Composable
+fun EditBodyCatatanPanen(
+    updateCatatanPanenUiState: UpdateCatatanPanenUiState,
+    onCtpnValueChange: (UpdateCatatanPanenUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputCatatanPanenEdit(
+            updateCatatanPanenUiEvent = updateCatatanPanenUiState.updateCatatanPanenUiEvent,
+            onValueChange = onCtpnValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF2CAC7C)
+            )
+        ) {
+            Text(text = "Simpan Perubahan")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
