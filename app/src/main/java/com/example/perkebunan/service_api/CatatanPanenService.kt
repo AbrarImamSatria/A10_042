f2ce905b1460cf87f2e106cf4fb0dae9773bedb0
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-23 09:41:00 +0700

    membuat interface CatatanPanenService

---
+++ app/src/main/java/com/example/perkebunan/service_api/CatatanPanenService.kt
@@ -0,0 +1,34 @@
+package com.example.perkebunan.service_api
+
+import com.example.perkebunan.model.CatatanPanen
+import com.example.perkebunan.model.Pekerja
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface CatatanPanenService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    @POST("insertcatatanpanen.php")
+    suspend fun insertCatatanPanen(@Body catatanPanen: CatatanPanen)
+
+    @GET("bacacatatanpanen.php")
+    suspend fun getAllCatatanPanen(): List<CatatanPanen>
+
+    @GET("baca1catatanpanen.php/{id_panen}")
+    suspend fun getCatatanPanenbyIdPanen(@Query("id_panen") idpanen:String): CatatanPanen
+
+    @PUT("editcatatanpanen.php/{id_panen}")
+    suspend fun updateCatatanPanen(@Query("id_panen")idPanen:String, @Body catatanPanen: CatatanPanen)
+
+    @DELETE("deletecatatanpanen.php/{id_panen}")
+    suspend fun deleteCatatanPanen(@Query("id_panen") idPanen:String): Response<Void>
+}
