546238e61d79fdb175487c13914ae4e9e01c290f
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-23 12:48:22 +0700

    membuat class UpdateCatatanPanenViewMode dan fun updateCatatanPanenUiState dan suspend fun updateCtpn

--- app/src/main/java/com/example/perkebunan/ui/viewmodel/catatanpanen/UpdateCatatanPanenViewModel.kt
+++ app/src/main/java/com/example/perkebunan/ui/viewmodel/catatanpanen/UpdateCatatanPanenViewModel.kt
@@ -1,6 +1,43 @@
 package com.example.perkebunan.ui.viewmodel.catatanpanen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.perkebunan.model.CatatanPanen
+import com.example.perkebunan.repository.CatatanPanenRepository
+import kotlinx.coroutines.launch
+
+class UpdateCatatanPanenViewModel(private val ctpn: CatatanPanenRepository) : ViewModel() {
+    var uiState by mutableStateOf(UpdateCatatanPanenUiState())
+        private set
+
+    fun updateCatatanPanenUiState(updateCatatanPanenUiEvent: UpdateCatatanPanenUiEvent) {
+        uiState = UpdateCatatanPanenUiState(updateCatatanPanenUiEvent = updateCatatanPanenUiEvent)
+    }
+
+    suspend fun updateCtpn(idPanen: String) {
+        viewModelScope.launch {
+            try {
+                ctpn.updateCatatanPanen(idPanen, uiState.updateCatatanPanenUiEvent.toCtpn())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    suspend fun getCatatanPanenbyIdPanen(idPanen: String) {
+        viewModelScope.launch {
+            try {
+                val result = ctpn.getCatatanPanenbyIdPanen(idPanen)
+                uiState = result.toUpdateCatatanPanenUiState()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class UpdateCatatanPanenUiState(
     val updateCatatanPanenUiEvent: UpdateCatatanPanenUiEvent = UpdateCatatanPanenUiEvent()
