bc97371e59a88addf48cf72473f928e5b112bbb6
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-23 15:51:39 +0700

    membuat private fun DetailItem

---
+++ app/src/main/java/com/example/perkebunan/ui/view/aktivitaspertanian/DetailAktivitasPertanianView.kt
@@ -0,0 +1,34 @@
+package com.example.perkebunan.ui.view.aktivitaspertanian
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+private fun DetailItem(
+    label: String,
+    value: String
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = value,
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
