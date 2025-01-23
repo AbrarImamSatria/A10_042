b8dfeebc2c213af7ed6fdf71d2178ac5b08c7c0c
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-23 14:14:19 +0700

    membuat fun AktCard

--- app/src/main/java/com/example/perkebunan/ui/view/aktivitaspertanian/HomeAktivitasPertanianView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/aktivitaspertanian/HomeAktivitasPertanianView.kt
@@ -1,10 +1,94 @@
 package com.example.perkebunan.ui.view.aktivitaspertanian
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.perkebunan.R
+import com.example.perkebunan.model.AktivitasPertanian
+
+@Composable
+fun AktCard(
+    aktivitasPertanian: AktivitasPertanian,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (AktivitasPertanian) -> Unit = {},
+){
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFECEFF1))
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.chemical),
+                contentDescription = "Gambar catatan panen",
+                modifier = Modifier.size(60.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = aktivitasPertanian.tanggalAktivitas,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontSize = 16.sp, fontWeight = FontWeight.Bold)
+                )
+                Text(
+                    text = aktivitasPertanian.deskripsiAktivitas,
+                    style = MaterialTheme.typography.bodyMedium.copy(color = Color.Gray,fontWeight = FontWeight.Bold)
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.info),
+                    contentDescription = "Info Detail",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clickable {}
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.bin),
+                    contentDescription = "Hapus Catatan Panen",
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clickable { onDeleteClick(aktivitasPertanian) }
+                )
+            }
+        }
+    }
+}
 
 @Composable
 private fun DeleteConfirmationDialog(
