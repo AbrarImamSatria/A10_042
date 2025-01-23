1b357138edcdb620859a18f2d208e7800a021e76
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-23 16:29:29 +0700

    membuat class UpdateAktivitasPertanianViewModel, fun updateAktivitasPertanianUiState dan suspend fun updateAkt

--- app/src/main/java/com/example/perkebunan/ui/viewmodel/aktivitaspertanian/UpdateAktivitasPertanianViewModel.kt
+++ app/src/main/java/com/example/perkebunan/ui/viewmodel/aktivitaspertanian/UpdateAktivitasPertanianViewModel.kt
@@ -1,6 +1,43 @@
 package com.example.perkebunan.ui.viewmodel.aktivitaspertanian
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.perkebunan.model.AktivitasPertanian
+import com.example.perkebunan.repository.AktivitasPertanianRepository
+import kotlinx.coroutines.launch
+
+class UpdateAktivitasPertanianViewModel(private val akt: AktivitasPertanianRepository) : ViewModel() {
+    var uiState by mutableStateOf(UpdateAktivitasPertanianUiState())
+        private set
+
+    fun updateAktivitasPertanianUiState(updateAktivitasPertanianUiEvent: UpdateAktivitasPertanianUiEvent) {
+        uiState = UpdateAktivitasPertanianUiState(updateAktivitasPertanianUiEvent = updateAktivitasPertanianUiEvent)
+    }
+
+    suspend fun updateAkt(idAktivitas: String) {
+        viewModelScope.launch {
+            try {
+                akt.updateAktivitasPertanian(idAktivitas, uiState.updateAktivitasPertanianUiEvent.toAkt())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    suspend fun getAktivitasPertanianbyIdAktivitas(idAktivitas: String) {
+        viewModelScope.launch {
+            try {
+                val result = akt.getAktivitasPertanianbyIdAktivitas(idAktivitas)
+                uiState = result.toUpdateAktivitasPertanianUiState()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class UpdateAktivitasPertanianUiState(
     val updateAktivitasPertanianUiEvent: UpdateAktivitasPertanianUiEvent = UpdateAktivitasPertanianUiEvent()
