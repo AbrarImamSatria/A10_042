f43fa43a065aa8c531fdc5888aef1e7371be8b42
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-23 12:17:50 +0700

    membuat fun EntryCtpnScreen dan object DestinasiEntryCatatanPanen

--- app/src/main/java/com/example/perkebunan/ui/view/catatanpanen/InsertCatatanPanenView.kt
+++ app/src/main/java/com/example/perkebunan/ui/view/catatanpanen/InsertCatatanPanenView.kt
@@ -4,19 +4,74 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.perkebunan.navigation.DestinasiNavigasi
+import com.example.perkebunan.ui.PenyediaViewModel
+import com.example.perkebunan.ui.customwidget.CostumeTopAppBar
 import com.example.perkebunan.ui.viewmodel.catatanpanen.InsertCatatanPanenUiEvent
 import com.example.perkebunan.ui.viewmodel.catatanpanen.InsertCatatanPanenUiState
+import com.example.perkebunan.ui.viewmodel.catatanpanen.InsertCatatanPanenViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiEntryCatatanPanen: DestinasiNavigasi {
+    override val route = "item_entryCatatanPanen"
+    override val titleRes = "Entry Catatan Panen"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryCtpnScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertCatatanPanenViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryCatatanPanen.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerPadding ->
+        EntryBody(
+            insertCatatanPanenUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertCtpnState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertCtpn()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntryBody(
