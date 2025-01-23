5b554f4f30affe112523308e788a893278e5f760
abrarimamsatria09 <092004abrar@gmail.com>
2025-01-23 13:25:04 +0700

    menambahkan viewmodel catatan panen ke PenyediaViewModel

--- app/src/main/java/com/example/perkebunan/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/perkebunan/ui/PenyediaViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.perkebunan.ui
 
-import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.perkebunan.PerkebunanApplications
+import com.example.perkebunan.ui.viewmodel.catatanpanen.DetailCatatanPanenViewModel
+import com.example.perkebunan.ui.viewmodel.catatanpanen.HomeCatatanPanenViewModel
+import com.example.perkebunan.ui.viewmodel.catatanpanen.InsertCatatanPanenViewModel
+import com.example.perkebunan.ui.viewmodel.catatanpanen.UpdateCatatanPanenViewModel
 import com.example.perkebunan.ui.viewmodel.pekerja.DetailPekerjaViewModel
 import com.example.perkebunan.ui.viewmodel.pekerja.HomePekerjaViewModel
 import com.example.perkebunan.ui.viewmodel.pekerja.InsertPekerjaViewModel
@@ -17,14 +20,23 @@ import com.example.perkebunan.ui.viewmodel.tanaman.UpdateTanamanViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
+        // VIEWMODEL TANAMAN
         initializer { HomeTanamanViewModel(aplikasiPerkebunan().container.tanamanRepository) }
         initializer { InsertTanamanViewModel(aplikasiPerkebunan().container.tanamanRepository) }
         initializer { DetailTanamanViewModel(aplikasiPerkebunan().container.tanamanRepository) }
         initializer { UpdateTanamanViewModel(aplikasiPerkebunan().container.tanamanRepository) }
+
+        // VIEWMODEL PEKERJA
         initializer { HomePekerjaViewModel(aplikasiPerkebunan().container.pekerjaRepository) }
         initializer { InsertPekerjaViewModel(aplikasiPerkebunan().container.pekerjaRepository) }
         initializer { DetailPekerjaViewModel(aplikasiPerkebunan().container.pekerjaRepository) }
         initializer { UpdatePekerjaViewModel(aplikasiPerkebunan().container.pekerjaRepository) }
+
+        // VIEWMODEL CATATAN PANEN
+        initializer { HomeCatatanPanenViewModel(aplikasiPerkebunan().container.catatanPanenRepository) }
+        initializer { InsertCatatanPanenViewModel(aplikasiPerkebunan().container.catatanPanenRepository) }
+        initializer { DetailCatatanPanenViewModel(aplikasiPerkebunan().container.catatanPanenRepository) }
+        initializer { UpdateCatatanPanenViewModel(aplikasiPerkebunan().container.catatanPanenRepository) }
     }
 
     fun CreationExtras.aplikasiPerkebunan():PerkebunanApplications =
